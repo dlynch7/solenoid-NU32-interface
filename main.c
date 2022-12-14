0d29e5aa767350b85f216ad6993dafe767c0e66e
dlynch7 <daniellynch2021@u.northwestern.edu>
2022-12-14 14:15:25 -0600

    added remaining menu options for interfacing with solenoid library

--- main.c
+++ main.c
@@ -8,24 +8,24 @@
 #define MAX_MESSAGE_LENGTH 200
 
 const char menu_str[] =	"\
-\t0: \r\n\
-\t1: \r\n\
-\t2: \r\n\
-\t3: \r\n\
-\t4: \r\n\
-\t5: \r\n\
-\t6: \r\n\
-\t7: \r\n\
-\t8: \r\n\
-\t9: \r\n";
+\t0: Solenoid OFF\r\n\
+\t1: Solenoid ON\r\n\
+\t2: Reduced-current holding OFF\r\n\
+\t3: Reduced-current holding ON\r\n\
+\t4: Get hold-phase PWM duty cycle\r\n\
+\t5: Set hold-phase PWM duty cycle\r\n\
+\t6: Get peak-current duration\r\n\
+\t7: Set peak-current duration\r\n";
 
 int main(void) {
 	char message[MAX_MESSAGE_LENGTH]; // for serial I/O
+	uint8_t hold_duty_cycle = 100;
+	uint16_t peak_duration_ms = 1E3;
   
 	NU32_Startup(); // cache on, interrupts on, LED/button init, UART init
 	NU32_WriteUART3("Initialized NU32.\r\n");
 	
-	// TODO: initialize solenoid interface
+	init_solenoid();
 	NU32_WriteUART3("Initialized solenoid interface.\r\n");
 
 	while (1) {
@@ -38,40 +38,56 @@ int main(void) {
 		NU32_WriteUART3(" from user.\r\n");			// carriage return and newline
 			
 		switch (message[0]) {
-			case '0': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '0': { // turn solenoid OFF
+				toggle_solenoid(SOLENOID_OFF);
+				NU32_WriteUART3("\tSolenoid OFF.\r\n");
 				break;
 			}
-			case '1': { // enable/disable motor
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '1': { // turn solenoid ON
+				toggle_solenoid(SOLENOID_ON);
+				NU32_WriteUART3("\tSolenoid ON.\r\n");
 				break;
 			}
-			case '2': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '2': { // disable reduced-current holding
+				toggle_current_reduction(0);
+				NU32_WriteUART3("\tReduced-current holding disabled.\r\n");
 				break;
 			}
-			case '3': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '3': { // enable reduced-current holding
+				toggle_current_reduction(1);
+				NU32_WriteUART3("\tReduced-current holding enabled.\r\n");
 				break;
 			}
-			case '4': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '4': { // get reduced-current PWM duty cycle
+				sprintf(message,"\tHold-phase PWM duty cycle is %hu %%.\r\n",\
+					get_hold_duty_cycle());
+				NU32_WriteUART3(message);
 				break;
 			}
-			case '5': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '5': { // set reduced-current PWM duty cycle
+				NU32_WriteUART3("Enter hold-phase PWM duty cycle [integer, 0-100]:\r\n");
+				NU32_ReadUART3(message,MAX_MESSAGE_LENGTH);
+				sscanf(message,"%hhu",&hold_duty_cycle);
+				set_hold_duty_cycle(hold_duty_cycle);
+				sprintf(message,"\tSet hold-phase PWM duty cycle to %hhu %%.\r\n",\
+					get_hold_duty_cycle());
+				NU32_WriteUART3(message);
 				break;
 			}
-			case '6': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '6': { // get peak-current duration
+				sprintf(message,"\tPeak-current duration: %hu ms.\r\n",\
+					get_peak_duration_ms());
+				NU32_WriteUART3(message);
 				break;
 			}
-			case '7': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
-				break;
-			}
-			case '8': { // TODO
-				// NU32_WriteUART3(".\r\n"); // TODO
+			case '7': { // set peak-current duration
+				NU32_WriteUART3("Enter peak-current duration [integer, milliseconds]:\r\n");
+				NU32_ReadUART3(message,MAX_MESSAGE_LENGTH);
+				sscanf(message,"%hu",&peak_duration_ms);
+				set_peak_duration_ms(peak_duration_ms);
+				sprintf(message,"\tSet peak-current duration to %hu ms.\r\n",\
+					get_peak_duration_ms());
+				NU32_WriteUART3(message);	
 				break;
 			}
 			default: { // Unrecognized option, handle error
